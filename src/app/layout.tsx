509c21e91f4eec2e335bfabd086cf77d47012d99
andrew <trolevan26299@gmail.com>
2025-01-15 00:47:51 +0700

    update code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
+import MainLayout from "@/layouts/main.layout";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import MainLayout from "@/layouts/main.layout";
-import AddToHomeScreen from "@/sections/addToHomeScreen";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,17 +14,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
   return (
     <html lang="en">
-       <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta name="theme-color" content="#000000" />
-        <link rel="apple-touch-icon" href="/public/vercel.svg" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-      </head>
       <body className={inter.className}>
       <script src="https://telegram.org/js/telegram-web-app.js"></script>
-          <AddToHomeScreen />
+         
           <MainLayout>{children}</MainLayout>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 "use client";
 
 import { DashboardView } from "@/sections/dashboard/view";
-import { useEffect } from "react";
 
 
 export default function Home() {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then((registration) => console.log('SW registered:', registration))
-        .catch((error) => console.log('SW registration failed:', error));
-    }
-  }, []);
   return <DashboardView />;
 }
