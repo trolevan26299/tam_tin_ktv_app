70c42e7d7a711ce5a988a4ba636ea346d3278638
Andrew <trolevan26299@gmail.com>
2025-03-10 19:15:20 +0700

    update

--- src/sections/scan/scan-view.tsx
+++ src/sections/scan/scan-view.tsx
@@ -8,9 +8,13 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Device } from "@/types/device.type";
 import dynamic from "next/dynamic";
-const DynamicQrScanner = dynamic(() => import("@yudiel/react-qr-scanner").then((mod) => mod.QrScanner), {
-  ssr: false,
-});
+import { format } from "date-fns";
+const DynamicQrScanner = dynamic(
+  () => import("@yudiel/react-qr-scanner").then((mod) => mod.QrScanner),
+  {
+    ssr: false,
+  }
+);
 
 export const ScanPage = () => {
   const router = useRouter();
@@ -53,7 +57,8 @@ export const ScanPage = () => {
     toast({
       variant: "destructive",
       title: "Lỗi camera",
-      description: "Không thể truy cập camera. Vui lòng kiểm tra quyền truy cập.",
+      description:
+        "Không thể truy cập camera. Vui lòng kiểm tra quyền truy cập.",
     });
   };
 
@@ -77,7 +82,11 @@ export const ScanPage = () => {
               Thông tin thiết bị
             </h1>
           )}
-          {!deviceHistory && <p className="text-gray-600 mt-2 font-medium">Đặt mã QR vào khung hình để quét</p>}
+          {!deviceHistory && (
+            <p className="text-gray-600 mt-2 font-medium">
+              Đặt mã QR vào khung hình để quét
+            </p>
+          )}
         </div>
         {!deviceHistory ? (
           <div className="relative aspect-square bg-white/50 backdrop-blur-sm rounded-2xl p-4 shadow-lg">
@@ -106,7 +115,9 @@ export const ScanPage = () => {
                     // Thêm cấu hình này
                     facingMode: "environment",
                   }}
-                  viewFinder={() => <div className="border-2 border-red-500 absolute top-0 left-0 w-full h-full" />}
+                  viewFinder={() => (
+                    <div className="border-2 border-red-500 absolute top-0 left-0 w-full h-full" />
+                  )}
                   containerStyle={{ borderRadius: "0.5rem", height: "100%" }}
                   videoStyle={{
                     borderRadius: "0.5rem",
@@ -120,7 +131,7 @@ export const ScanPage = () => {
           </div>
         ) : (
           <Card className="p-3 bg-white/80 backdrop-blur-sm shadow-xl border-0 animate-fade-in">
-            <div className="space-y-6 max-h-[75vh] overflow-y-auto pr-2">
+            <div className="space-y-3 max-h-[75vh] overflow-y-auto pr-2">
               {/* Thông tin cơ bản */}
               <div className="grid gap-3">
                 <div className="flex  gap-1">
@@ -138,68 +149,97 @@ export const ScanPage = () => {
                   </span>
                 </div>
                 <div className="flex flex-row justify-between items-center">
-
-                <div className="flex flex-row justify-between items-center">
-                  <span className="font-medium min-w-20">Bảo hành:</span>
-                  <span>{deviceHistory.warranty}T</span>
-                </div>
-                <div className="flex flex-row justify-between items-center">
-                  <span className="font-medium min-w-24">Ngày mua:</span>
-                  <span>{deviceHistory.date_buy}</span>
-                </div>
+                  <div className="flex flex-row justify-between items-center">
+                    <span className="font-medium min-w-20">Bảo hành:</span>
+                    <span>{deviceHistory.warranty}T</span>
+                  </div>
+                  <div className="flex flex-row justify-between items-center">
+                    <span className="font-medium min-w-24">Ngày mua:</span>
+                    <span>{deviceHistory.date_buy}</span>
+                  </div>
                 </div>
               </div>
 
               {/* Thông tin khách hàng */}
-              <div className="border-t pt-2">
+              <div className="border-t pt-1">
                 <h3 className="font-medium mb-2">Thông tin khách hàng:</h3>
                 <div className="grid gap-2">
                   <div className="flex items-center gap-2">
-                    <span className="font-medium min-w-32">Loại khách hàng:</span>
+                    <span className="font-medium min-w-32">
+                      Loại khách hàng:
+                    </span>
                     <span>{deviceHistory.type_customer}</span>
                   </div>
                   <div className="flex  gap-2">
-                    <span className="font-medium min-w-32">Tên khách hàng:</span>
+                    <span className="font-medium min-w-32">
+                      Tên khách hàng:
+                    </span>
                     <span>{deviceHistory.name_customer}</span>
                   </div>
                 </div>
               </div>
 
               {/* Lịch sử sửa chữa */}
-              <div className="border-t pt-2">
+              <div className="border-t pt-1">
                 <h3 className="font-medium mb-3">Lịch sử sửa chữa</h3>
                 <div className="space-y-4">
-                  {deviceHistory.history_repair?.map((repair: any, index: number) => (
-                    <div key={index} className="bg-gray-50 rounded-lg p-4">
-                      <div className="flex justify-between items-center mb-2">
-                        <span className="font-medium">{repair.type_repair}</span>
-                        <span className="text-sm text-gray-500">{repair.date_repair}</span>
-                      </div>
-                      <div className="mb-2">
-                        <span className="text-sm font-medium">Nhân viên sửa chữa: </span>
-                        <span className="text-sm text-gray-600">{repair.staff_repair || "Chưa có thông tin"}</span>
-                      </div>
-
-                      {/* Danh sách linh kiện */}
-                      <div className="mb-2">
-                        <p className="text-sm font-medium mb-1">Linh kiện thay thế:</p>
-                        <div className="grid gap-1">
-                          {repair.linh_kien.map((lk: any, lkIndex: number) => (
-                            <div key={lkIndex} className="text-sm flex justify-between">
-                              <span>{lk.name}</span>
-                              <span className="text-gray-600">Số lượng: {lk.total}</span>
-                            </div>
-                          ))}
+                  {deviceHistory.history_repair?.map(
+                    (repair: any, index: number) => (
+                      <div key={index} className="bg-gray-50 rounded-lg p-4">
+                        <div className="flex justify-between items-center mb-2">
+                          <span className="font-medium">
+                            {repair.type_repair}
+                          </span>
+                          <span>
+                            {deviceHistory.date_buy
+                              ? format(
+                                  new Date(deviceHistory.date_buy),
+                                  "dd/MM/yyyy"
+                                )
+                              : ""}
+                          </span>
                         </div>
-                      </div>
+                        <div className="mb-2">
+                          <span className="text-sm font-medium">
+                            Nhân viên sửa chữa:{" "}
+                          </span>
+                          <span className="text-sm text-gray-600">
+                            {repair.staff_repair || "Chưa có thông tin"}
+                          </span>
+                        </div>
 
-                      {/* Ghi chú */}
-                      <div className="text-sm">
-                        <span className="font-medium">Ghi chú: </span>
-                        <span className="text-gray-600">{repair.note}</span>
+                        {/* Danh sách linh kiện */}
+                        <div className="mb-2">
+                          <p className="text-sm font-medium mb-1">
+                            Linh kiện thay thế:
+                          </p>
+                          <div className="grid gap-1">
+                            {repair.linh_kien.map(
+                              (lk: any, lkIndex: number) => (
+                                <div
+                                  key={lkIndex}
+                                  className="text-sm flex justify-between"
+                                >
+                                  <span>{lk.name}</span>
+                                  <span className="text-gray-600">
+                                    Số lượng: {lk.total}
+                                  </span>
+                                </div>
+                              )
+                            )}
+                          </div>
+                        </div>
+
+                        {/* Ghi chú */}
+                        {repair.note && (
+                          <div className="text-sm">
+                            <span className="font-medium">Ghi chú: </span>
+                            <span className="text-gray-600">{repair.note}</span>
+                          </div>
+                        )}
                       </div>
-                    </div>
-                  ))}
+                    )
+                  )}
                 </div>
               </div>
             </div>
